7b44de4fb8162a36ad0fbc8cdf148e6ccccfd6c8
Anubhavjain16 <103402079+Anubhavjain16@users.noreply.github.com>
2024-08-18 11:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,23 +16,23 @@ with col1:
     height = st.number_input("Height (cm)", min_value=0)
     weight = st.number_input("Weight (kg)", min_value=0)
     age = st.number_input("Age", min_value=0)
-    gender = st.selectbox("Gender", ["Male", "Female", "Other"])
+    gender = st.selectbox("Gender", ["Male", Female", "Other"])
     present_complaints = st.text_area("Present Complaints")
 
 # Second page: RIVE Factors
 with col2:
     st.subheader("RIVE Factors")
     
-    # RIVE factors form
-    rf1 = st.checkbox("RF1")
-    rf2 = st.checkbox("RF2")
-    rf3 = st.checkbox("RF3")
-    rf4 = st.checkbox("RF4")
-    rf5 = st.checkbox("RF5")
-    rf6 = st.checkbox("RF6")
-    rf7 = st.checkbox("RF7")
-    rf8 = st.checkbox("RF8")
-    rf9 = st.checkbox("RF9")
+    # RIVE factors form with input boxes
+    rf1 = st.text_input("RF1")
+    rf2 = st.text_input("RF2")
+    rf3 = st.text_input("RF3")
+    rf4 = st.text_input("RF4")
+    rf5 = st.text_input("RF5")
+    rf6 = st.text_input("RF6")
+    rf7 = st.text_input("RF7")
+    rf8 = st.text_input("RF8")
+    rf9 = st.text_input("RF9")
     calcification_score = st.number_input("Calcification Score", min_value=0)
 
     if st.button("Submit"):
